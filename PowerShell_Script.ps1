6795194e1e733c3dd478a72367b3ad145ee726c6
marouAna <maria.anastam@gmail.com>
2022-07-13 16:29:41 +0100

    Create a power shell script file

---
+++ PowerShell_Script.ps1
@@ -0,0 +1,1 @@
+Invoke-WebRequest -SkipCertificateCheck -Uri "https://snirh.pt/snirh/_dadosbase/site/paraCSV/dados_csv.php?sites=920685260&pars=1436794570,1520200094&tmin=01/10/1965&tmax=10/03/2022&formato=csv" -Outfile "dados.csv"
